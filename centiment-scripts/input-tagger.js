f5cdaffa4e51defddaf9191e6bddc38498990fa3
Moin Khan <mo.inkhan@outlook.com>
2020-11-20 15:07:00 +0530

    Updated: Centiment Input Tagger

--- centiment-scripts/input-tagger.js
+++ centiment-scripts/input-tagger.js
@@ -9,23 +9,23 @@
 //Initialize params
 var LocPath = window.location.pathname;
 var inputCustom = document.createElement("input");
-var targetForm = ''
 inputCustom.type = "text";
 inputCustom.name = "contact-source"
 inputCustom.style = "display: none";
+var targetForm = ''
 
 //Hero Input
 targetForm = document.getElementById("wf-form-Hero")
 if (targetForm) {
     inputCustom.value = "cta-email-hero";
-    document.getElementById("wf-form-Hero").appendChild(inputCustom);
+    targetForm.appendChild(inputCustom);
 }
 
 //Form Input
 targetForm = document.getElementById("cta-form-footer")
 if (targetForm) {
     inputCustom.value = "cta-form-footer";
-    document.getElementById("cta-form-footer").appendChild(inputCustom);
+    targetForm.appendChild(inputCustom);
 }
 
 //Survey Tool
@@ -33,7 +33,7 @@ if (LocPath == '/survey-tool') {
     targetForm = document.getElementById("cta-form-surveytool")
     if (targetForm) {
         inputCustom.value = "cta-email-surveytool";
-        document.getElementById("cta-form-surveytool").appendChild(inputCustom);
+        targetForm.appendChild(inputCustom);
     }
 }
 
@@ -42,7 +42,7 @@ if (LocPath == '/audience-panel') {
     targetForm = document.getElementById("wf-form-Survey")
     if (targetForm) {
         inputCustom.value = "cta-email-audiencePanel";
-        document.getElementById("wf-form-Survey").appendChild(inputCustom);
+        targetForm.appendChild(inputCustom);
     }
 }
 
@@ -51,7 +51,7 @@ if (LocPath == '/customers') {
     targetForm = document.getElementById("wf-form-Customers")
     if (targetForm) {
         inputCustom.value = "cta-email-customers";
-        document.getElementById("wf-form-Customers").appendChild(inputCustom);
+        targetForm.appendChild(inputCustom);
     }
 
 }
