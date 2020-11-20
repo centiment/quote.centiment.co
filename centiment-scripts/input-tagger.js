84feccb6a548147f4b328fdfedd65561afdca83a
Moin Khan <mo.inkhan@outlook.com>
2020-11-20 14:08:51 +0530

    Updated: Centiment Input Tagger

--- centiment-scripts/input-tagger.js
+++ centiment-scripts/input-tagger.js
@@ -9,19 +9,20 @@
 //Initialize params
 var LocPath = window.location.pathname;
 var inputCustom = document.createElement("input");
+var targetForm = ''
 inputCustom.type = "text";
 inputCustom.name = "contact-source"
 inputCustom.style = "display: none";
 
 //Hero Input
-var targetForm = document.getElementById("wf-form-Hero")
+targetForm = document.getElementById("wf-form-Hero")
 if (targetForm) {
     inputCustom.value = "cta-email-hero";
     targetForm.appendChild(inputCustom);
 }
 
 //Form Input
-var targetForm = document.getElementById("cta-form-footer")
+targetForm = document.getElementById("cta-form-footer")
 if (targetForm) {
     inputCustom.value = "cta-form-footer";
     targetForm.appendChild(inputCustom);
@@ -29,7 +30,7 @@ if (targetForm) {
 
 //Survey Tool
 if (LocPath == '/survey-tool') {
-    var targetForm = document.getElementById("cta-form-surveytool")
+    targetForm = document.getElementById("cta-form-surveytool")
     if (targetForm) {
         inputCustom.value = "cta-email-surveytool";
         targetForm.appendChild(inputCustom);
@@ -38,7 +39,7 @@ if (LocPath == '/survey-tool') {
 
 //Audience Panel
 if (LocPath == '/audience-panel') {
-    var targetForm = document.getElementById("wf-form-Survey")
+    targetForm = document.getElementById("wf-form-Survey")
     if (targetForm) {
         inputCustom.value = "cta-email-audiencePanel";
         targetForm.appendChild(inputCustom);
@@ -47,7 +48,7 @@ if (LocPath == '/audience-panel') {
 
 //Customers
 if (LocPath == '/customers') {
-    var targetForm = document.getElementById("wf-form-Customers")
+    targetForm = document.getElementById("wf-form-Customers")
     if (targetForm) {
         inputCustom.value = "cta-email-customers";
         targetForm.appendChild(inputCustom);
@@ -58,7 +59,7 @@ if (LocPath == '/customers') {
 
 //MOE
 if (LocPath == '/margin-of-error') {
-    var targetForm = document.getElementById("wf-form-Sidebar")
+    targetForm = document.getElementById("wf-form-Sidebar")
     if (targetForm) {
         inputCustom.value = "cta-email-sidebar";
         targetForm.appendChild(inputCustom);
