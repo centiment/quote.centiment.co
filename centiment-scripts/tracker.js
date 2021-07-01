cd01a4221766643e8c5b141d12435d1d6c859a1f
Moin Khan <mo.inkhan@outlook.com>
2021-07-01 20:38:46 +0530

    ✨ New LPs added

--- centiment-scripts/tracker.js
+++ centiment-scripts/tracker.js
@@ -8,10 +8,10 @@
 
 /**
  * Returns value of a cookie otherwise an empty string
- * 
+ *
  * @param {string} cookieName Name of the cookie to be read
- * 
- * @returns {string} 
+ *
+ * @returns {string}
  */
 function getCookie(cookieName) {
     var result = document.cookie.match('(^|;)\\s*' + cookieName + '\\s*=\\s*([^;]+)')
@@ -21,17 +21,27 @@ function getCookie(cookieName) {
 
 
 /**
- * Obtain Tracking Parameters as a JSON String 
- * 
+ * Obtain Tracking Parameters as a JSON String
+ *
  * @param {string} cookieValue Stored Cookie Value
  * @param {string} queryString Query String of URL
- * 
- * @returns {string} 
+ *
+ * @returns {string}
  */
 function trackingValue(cookieValue = '', queryString = location.search.slice(1)) {
 
     if (cookieValue != '') {
         trackerValue = JSON.parse(cookieValue)
+    }
+
+    //Append Hutk
+    if (getCookie('hubspotutk')) {
+        trackerValue['hutk'] = getCookie('hubspotutk')
+    }
+
+    //Append First Page
+    if (!trackerValue.hasOwnProperty('firstPage')) {
+        trackerValue['firstPage'] = [encodeURIComponent(currentURL), time]
     }
 
     //Append query string
@@ -41,16 +51,14 @@ function trackingValue(cookieValue = '', queryString = location.search.slice(1))
             var pair = p.split('=')
             var key = pair[0]
             var value = decodeURIComponent(pair[1] || '');
-            if (!reservedKeys.includes(key)) {
+
+            if (!reservedKeys.includes(key)
+                && (Object.keys(trackerValue).length < 10 || trackerValue.hasOwnProperty(key) || key.includes("utm_"))) {
                 trackerValue[key] = value
             }
         });
     }
 
-    //Append Hutk
-    if(getCookie('hubspotutk')){
-        trackerValue['hutk'] = getCookie('hubspotutk')
-    }
 
     //Append centiment current URL
     if (currentURL) {
@@ -59,27 +67,36 @@ function trackingValue(cookieValue = '', queryString = location.search.slice(1))
         if (!Array.isArray(trackerValue['flow-tracker'])) {
             trackerValue['flow-tracker'] = []
         }
-        trackerValue['flow-tracker'].push(item)
 
+        while (Object.keys(trackerValue['flow-tracker']).length > 14) {
+            trackerValue['flow-tracker'].shift()
+        }
+
+        trackerValue['flow-tracker'].push(item)
+    }
+    if (typeof (trackerValue['flow-tracker']) == 'undefined') {
+        trackerValue['flow-tracker'] = ['none', time]
     }
 
-    //Append non-centiment referrers   
+
+    //Append non-centiment referrers
     if (referrerDomain && !centimentDomains.includes(referrerDomain)) {
         var referrer_item = [encodeURIComponent(referrerURL), time]
 
         if (!Array.isArray(trackerValue['centiment-referrer'])) {
             trackerValue['centiment-referrer'] = []
         }
+
+        while (Object.keys(trackerValue['centiment-referrer']).length > 9) {
+            trackerValue['centiment-referrer'].shift()
+        }
+
         trackerValue['centiment-referrer'].push(referrer_item)
     }
-
-    if (typeof (trackerValue['flow-tracker']) == 'undefined') {
-        trackerValue['flow-tracker'] = ['none', time]
-    }
-
     if (typeof (trackerValue['centiment-referrer']) == 'undefined') {
         trackerValue['centiment-referrer'] = ['none', time]
     }
+
 
     return JSON.stringify(trackerValue)
 };
@@ -89,14 +106,14 @@ function trackingValue(cookieValue = '', queryString = location.search.slice(1))
 
 /**
  * Set Cookie for 30 days
- * 
+ *
  * @param {string} value Cookie value to be set
- * 
+ *
  * @param {string} cookie_name Cookie name to be set. Default: CentimentTracker
- * 
+ *
  * @returns {none}
  */
-function setTrackingCookie(value, cookie_name = 'CentimentTracker' ) {
+function setTrackingCookie(value, cookie_name = 'CentimentTracker') {
     var cookie = cookie_name + '=' + value + '; path=/; domain=.centiment.co'
     var now = new Date()
     now.setTime(now.getTime() + 30 * 24 * 60 * 60 * 1000)
@@ -108,17 +125,16 @@ function setTrackingCookie(value, cookie_name = 'CentimentTracker' ) {
 var date = new Date();
 var time = date.getTime();
 
-var reservedKeys = ["flow-tracker", "centiment-referrer"];
+var reservedKeys = ["flow-tracker", "centiment-referrer", "firstPage"];
 
-var centimentDomains = ["www.centiment.co", "centiment.co", "quote.centiment.co", "temp.centiment.co", "app.centiment.co"]
+var centimentDomains = ["www.centiment.co", "centiment.co", "quote.centiment.co", "temp.centiment.co", "app.centiment.co", "assets.centiment.co"]
 
 var currentURL = window.location.href;
 
-var referrerURL = document.referrer;
-var referrerDomain = ""
-
 var trackerValue = {}
 
+var referrerURL = document.referrer;
+var referrerDomain = ""
 if (referrerURL) {
     referrerDomain = referrerURL.match(/:\/\/(.[^/]+)/)[1];
 }
